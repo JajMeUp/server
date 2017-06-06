62ae96e552dcba080a2471156eb157dc58f2f30f
Anthony Gourd <anthonygourd@gmail.com>
2017-06-06 22:29:56 +0200

    Initial commit

---
+++ src/main/java/com/jajteam/jajmeup/JajMeUp.java
@@ -0,0 +1,12 @@
+package com.jajteam.jajmeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JajMeUp {
+
+    public static void main(String args[]) {
+        SpringApplication.run(JajMeUp.class, args);
+    }
+}
