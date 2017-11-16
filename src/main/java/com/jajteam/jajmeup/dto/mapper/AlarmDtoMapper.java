34b229694b8eb281ff724c32965b9a6f7a61085c
valoulou <valentin.chaigneau@gmail.com>
2017-11-15 20:12:14 -0500

    GetAlarm fix display name

--- src/main/java/com/jajteam/jajmeup/dto/mapper/AlarmDtoMapper.java
+++ src/main/java/com/jajteam/jajmeup/dto/mapper/AlarmDtoMapper.java
@@ -9,7 +9,7 @@ public class AlarmDtoMapper {
         AlarmDto dto = new AlarmDto();
         dto.setMessage(alarm.getMessage());
         dto.setLink(alarm.getLink());
-        dto.setVoterName(alarm.getVoter().getUser().getUsername());
+        dto.setVoterName(alarm.getVoter().getUser().getProfile().getDisplayName());
 
         return dto;
     }
